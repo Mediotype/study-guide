72c2e42a2d27c759dd399589d48e4b81ed9f12db
Matt Sherer <matt@mediotype.com>
2019-07-01 09:13:30 -0500

    SG-1 - Committing study guide examples

---
+++ app/design/frontend/Mediotype/Base/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author    Mediotype Development <diveinto@mediotype.com>
+ * @copyright 2019 Mediotype. All Rights Reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/mediotype/base',
+    __DIR__
+);
